dd96ad826ff166323322d3f7a050b1153b9f2bf0
Adam Buczek <adambuczek@gmail.com>
2026-04-30 19:51:38 +0200

    split server and remove all dependencies on sibling packages

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io/fs"
 	"log"
-	"net/http"
-	"path/filepath"
-	"time"
 
 	"github.com/adambuczek/go-ssg/builder"
 	"github.com/adambuczek/go-ssg/config"
-	"github.com/fsnotify/fsnotify"
+	"github.com/adambuczek/go-ssg/server"
 )
 
 func main() {
@@ -33,88 +28,13 @@ func main() {
 		log.Fatal(err)
 	}
 	if dev {
-		go watch(cfg)
+		go server.Watch(cfg.Src, cfg.PollingTimeout, func() error {
+			return builder.Build(cfg, true)
+		})
 
 		// dev server
-		if err := serve(cfg); err != nil {
+		if err := server.Serve(cfg.Port, cfg.Dist); err != nil {
 			log.Fatal(err)
 		}
 	}
-}
-
-// dev server
-var reloadCh = make(chan struct{}, 1)
-
-func watch(cfg config.Config) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	filepath.WalkDir(cfg.Src, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			log.Printf("problem adding %s to watched directories: %s", path, err)
-			return filepath.SkipDir
-		}
-		if d.IsDir() {
-			err := watcher.Add(path)
-			if err != nil {
-				log.Printf("problem adding %s to watched directories: %s", path, err)
-			}
-		}
-		return nil
-	})
-
-	timer := time.NewTimer(0)
-	<-timer.C
-
-	for {
-		select {
-		case _, ok := <-watcher.Events:
-			if !ok {
-				return
-			}
-			timer.Reset(cfg.PollingTimeout)
-
-		case <-timer.C:
-			log.Println("rebuilding...")
-			if err := builder.Build(cfg, true); err != nil {
-				log.Println("build error:", err)
-			}
-			select {
-			case reloadCh <- struct{}{}:
-			default:
-			}
-
-		case err, ok := <-watcher.Errors:
-			if !ok {
-				return
-			}
-			log.Println(err)
-		}
-	}
-}
-
-func sseHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-
-	select {
-	case <-reloadCh:
-		fmt.Fprintf(w, "data: reload\n\n")
-		if f, ok := w.(http.Flusher); ok {
-			f.Flush()
-		}
-	case <-r.Context().Done():
-	}
-}
-
-func serve(cfg config.Config) error {
-	addr := ":" + cfg.Port
-	http.Handle("/", http.FileServer(http.Dir(cfg.Dist)))
-	http.HandleFunc("/_reload", sseHandler)
-	log.Printf("serving at http://localhost%s", addr)
-	return http.ListenAndServe(addr, nil)
 }
---
+++ server/server.go
@@ -0,0 +1,89 @@
+// Package server creates a dev server with watcher
+package server
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+var reloadCh = make(chan struct{}, 1)
+
+func Watch(src string, timeout time.Duration, build func() error) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	filepath.WalkDir(src, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Printf("problem adding %s to watched directories: %s", path, err)
+			return filepath.SkipDir
+		}
+		if d.IsDir() {
+			err := watcher.Add(path)
+			if err != nil {
+				log.Printf("problem adding %s to watched directories: %s", path, err)
+			}
+		}
+		return nil
+	})
+
+	timer := time.NewTimer(0)
+	<-timer.C
+
+	for {
+		select {
+		case _, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			timer.Reset(timeout)
+
+		case <-timer.C:
+			log.Println("rebuilding...")
+			if err := build(); err != nil {
+				log.Println("build error:", err)
+			}
+			select {
+			case reloadCh <- struct{}{}:
+			default:
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println(err)
+		}
+	}
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	select {
+	case <-reloadCh:
+		fmt.Fprintf(w, "data: reload\n\n")
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		}
+	case <-r.Context().Done():
+	}
+}
+
+func Serve(port string, dist string) error {
+	addr := ":" + port
+	http.Handle("/", http.FileServer(http.Dir(dist)))
+	http.HandleFunc("/_reload", sseHandler)
+	log.Printf("serving at http://localhost%s", addr)
+	return http.ListenAndServe(addr, nil)
+}
